f3598e3aa39c738be4d77d42582df4141d83adec
mdheerajprdxn <dheerajm@prdxn.com>
2020-11-18 12:24:08 +0530

    [js] slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,38 @@
 
 */
 
-console.log("hi");
+let leftSlide = document.querySelector("#left-slide");
+let rightSlide = document.querySelector("#right-slide");
+
+var imageSlider = document.querySelector(".slider-images");
+var images = document.querySelectorAll(".slider-images img");
+
+rightSlide.addEventListener("click", slideRight);
+leftSlide.addEventListener("click", slideLeft);
+
+counter = 0;
+
+let size = images[0].clientWidth;
+
+// imageSlider.style.transform = "translateX(" + -size * counter + "px)";
+
+function slideRight(e) {
+  e.preventDefault();
+  imageSlider.style.transition = "transform .4s ease-in-out";
+  counter++;
+  if (counter >= images.length) {
+    counter = 0;
+  }
+  imageSlider.style.transform = "translateX(" + -size * counter + "px)";
+}
+
+function slideLeft(e) {
+  e.preventDefault();
+  console.log(counter);
+  counter--;
+  if (counter < 0) {
+    counter = images.length - 1;
+  }
+  imageSlider.style.transition = "transform .4s ease-in-out";
+  imageSlider.style.transform = "translateX(" + -size * counter + "px)";
+}
