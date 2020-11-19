5214f6030395dd1c6707ddf1fef74749dfb86fa0
mdheerajprdxn <dheerajm@prdxn.com>
2020-11-19 12:22:30 +0530

    [js] second slider done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,24 @@
 
 */
 
+// Hamburger
+let hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector("nav ul");
+let backdrop = document.querySelector(".backdrop");
+hamburger.addEventListener("click", showNav);
+backdrop.addEventListener("click", hideNav);
+
+function showNav() {
+  nav.style.transform = "translateX(0)";
+  backdrop.style.display = "block";
+}
+
+function hideNav() {
+  nav.style.transform = "translateX(300px)";
+  backdrop.style.display = "none";
+}
+
+// Image Slider
 let leftSlide = document.querySelector("#left-slide");
 let rightSlide = document.querySelector("#right-slide");
 
@@ -40,7 +58,46 @@ function slideLeft(e) {
   imageSlider.style.transform = "translateX(" + -size * counter + "px)";
 }
 
-var videoImage = document.querySelector(".slider-videos img");
+// Video Slider
+let videoNext = document.getElementById("video-next");
+let videoPrev = document.getElementById("video-prev");
+
+var videoSlider = document.querySelector(".slider-videos");
+var videos = document.querySelectorAll(".slider-videos img");
+
+videoNext.addEventListener("click", videoSlideRight);
+videoPrev.addEventListener("click", videoSlideLeft);
+
+counter2 = 0;
+
+let size2 = videos[0].clientWidth;
+
+// imageSlider.style.transform = "translateX(" + -size * counter + "px)";
+
+function videoSlideRight(e) {
+  e.preventDefault();
+  videoSlider.style.transition = "transform .4s ease-in-out";
+  counter2++;
+  if (counter2 >= videos.length) {
+    counter2 = 0;
+  }
+
+  size = images[counter2].clientWidth;
+  videoSlider.style.transform = "translateX(" + -size2 * counter2 + "px)";
+}
+
+function videoSlideLeft(e) {
+  e.preventDefault();
+  counter2--;
+  if (counter2 < 0) {
+    counter2 = videos.length - 1;
+  }
+  videoSlider.style.transition = "transform .4s ease-in-out";
+  videoSlider.style.transform = "translateX(" + -size2 * counter2 + "px)";
+}
+
+// Play Video
+var videoImage = document.querySelector(".slider-videos");
 let video = document.querySelector(".testimonial video");
 
 videoImage.addEventListener("click", playVideo);
